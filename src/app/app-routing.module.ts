913a1725e1e367e9880cda17f2db30130e4c6d81
chaosRobe <dominik.panasz@outlook.com>
2026-05-20 08:56:13 +0200

    z4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FruitListComponent } from './fruit-list/fruit-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{ path: 'owoce', component: FruitListComponent },
+
+
+{ path: '', redirectTo: '/owoce', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/fruit-list/fruit-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Fruit } from '../fruit';
+import { FruitService } from '../fruit.service';
+
+@Component({
+  selector: 'app-fruit-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fruit-list.component.html',
+})
+export class FruitListComponent implements OnInit {
+
+  fruits: Fruit[] = [];
+
+ 
+  constructor(private fruitService: FruitService) {}
+
+ 
+  ngOnInit(): void {
+    this.loadFruits();
+  }
+
+ 
+  loadFruits(): void {
+    this.fruits = this.fruitService.getFruits();
+  }
+
+  
+  deleteFruit(id: number): void {
+    this.fruitService.deleteFruit(id); 
+    this.loadFruits();                
+  }
+}
